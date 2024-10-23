e4660d995eac1d0a35e99ed7d5849d4ef5db0ebe
Matteo Belletti <48958243+TeoB2@users.noreply.github.com>
2024-10-23 23:20:46 +0200

    Changed database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db" #sqlite
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Formula1?@localhost/TodoApplicationServer" #postgres
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Formula1?@localhost/TodoApplicationServer" #postgres - lovalhost
+SQLALCHEMY_DATABASE_URL = "postgresql://matteo:W9a1HBZ3erh6emjSY6mJOGfc5rs9rBgN@dpg-cscmf1jqf0us73eorjbg-a.frankfurt-postgres.render.com/pythonflashapicourse" #postgres - render
 #SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Formula1?@127.0.0.1:3306/TodoApplicationServer" #mysql
 
 #engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) #sqlite
